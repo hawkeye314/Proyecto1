93f7a88c81dd19b525ab39d2ad3a309710faf75b
Armando Garcia <89110363+hawkeye314@users.noreply.github.com>
2022-03-15 03:55:04 +0000

    Agregue la función para leer un archivo

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import 	java.io.*;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)throws IOException {
+    System.out.println("------- Proyecto 1 EDA -------");
+		File doc = new File("Demo.txt");
+		doc.createNewFile();
+		FileReader freader = new FileReader(doc);
+		char [] i = new char[100];
+		freader.read(i);
+		for(char j : i){
+			System.out.print(j);
+		}
+		freader.close();
   }
 }
