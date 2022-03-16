dba7ae2a7fae357a727044c0caf5867c93c7d4c8
Armando Garcia <89110363+hawkeye314@users.noreply.github.com>
2022-03-16 16:42:48 +0000

    Se agrego la opción de nombrar el archivo que se crea

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
-import 	java.io.*;
+import java.io.*;
+import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args)throws IOException {
-    System.out.println("------- Proyecto 1 EDA -------");
-		File doc = new File("Demo.txt");
-		doc.createNewFile();
-		FileReader freader = new FileReader(doc);
-		char [] i = new char[100];
-		freader.read(i);
-		for(char j : i){
-			System.out.print(j);
-		}
-		freader.close();
+      public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        System.out.println("------- Proyecto 1 EDA -------");
+        System.out.println("Dame el nombre del archivo de caracteres");
+        nombre=sc.nextLine();
+        File doc = new File(nombre+".txt");
+        doc.createNewFile();
+        FileReader freader = new FileReader(doc);
+        char [] i = new char[100];
+        freader.read(i);
+        for(char j : i){
+                System.out.print(j);
+        }
+        freader.close();
   }
 }
